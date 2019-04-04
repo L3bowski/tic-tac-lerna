4840696cbc31b924ed0e729ea33d13aff96b4f04
Carles Capellas <capellas.carles@gmail.com>
2019-04-04 16:33:54 +0200

    Wrap tic-tac-lerna-main code into an exported function

--- packages/tic-tac-lerna-main/src/index.ts
+++ packages/tic-tac-lerna-main/src/index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 import { getGrids } from 'tic-tac-lerna-grid';
 
-const port = 5000;
-const server = express();
+const startServer = () => {
+    const port = 5000;
+    const server = express();
 
-server.use(express.static('public'));
+    server.use(express.static('public'));
 
-server.use(/^\/grids$/, (req, res, next) => {
-    return res.send(JSON.stringify(getGrids()));
-});
+    server.use(/^\/grids$/, (req, res, next) => {
+        return res.send(JSON.stringify(getGrids()));
+    });
 
-server.listen(port, (error: any) => {
-    console.log(error || `Sever running in port ${port}`)
-})
+    server.listen(port, (error: any) => {
+        console.log(error || `Sever running in port ${port}`)
+    });
+}
+
+export {
+    startServer
+};
