fed3e4f3e168b5db3b58ec4e3b6c2adefff032ae
Carles Capellas <capellas.carles@gmail.com>
2019-04-04 16:19:37 +0200

    Add stub method to test lerna changes

--- packages/tic-tac-lerna-grid/src/index.ts
+++ packages/tic-tac-lerna-grid/src/index.ts
@@ -2,6 +2,11 @@ const getGrids = (take?: number, skip?: number): any[] => {
     return [];
 };
 
+const getGridById = (id: number): any => {
+    return {};
+};
+
 export {
+    getGridById,
     getGrids
 };
