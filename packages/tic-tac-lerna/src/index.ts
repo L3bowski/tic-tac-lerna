afad8d004c9a591dfe756257b15ec248bd975004
Carles Capellas <carles.capellas@sage.com>
2019-05-28 16:56:59 +0200

    Another dummy commit

--- packages/tic-tac-lerna/src/index.ts
+++ packages/tic-tac-lerna/src/index.ts
@@ -1,7 +1,7 @@
-import path from 'path';
+import { join } from 'path';
 import { startServer } from 'tic-tac-lerna-main';
 
-const publicFolderPath = path.join(__dirname, '..', 'node_modules', 'tic-tac-lerna-main', 'public');
+const publicFolderPath = join(__dirname, '..', 'node_modules', 'tic-tac-lerna-main', 'public');
 startServer(publicFolderPath);
 
 console.log('Irrelevant change to test Lerna independent versioning');
